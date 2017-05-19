15acfd616275d236a6badf12d3666f489a7b01be
lefreire <letfreirefigueiredo@gmail.com>
2017-05-19 17:09:49 -0300

    Testing clauses

--- criaAmbientev2.pl
+++ criaAmbientev2.pl
@@ -2,11 +2,13 @@
 :- ensure_loaded(mina).
 
 
-
 preparaJogo(ToWrite):-
 	/*find all minas em Minas*/
+	tabuleiro(TableSize),
 	findall([X,Y], mina(X,Y),Minas),
-	write(ToWrite, Minas).
+	write(ToWrite, Minas),
+	write(ToWrite, TableSize).
+	/*find table size */
 	/*preenche o tab de zeros a menos que seja mina - iniciaTabuleiro*/
 	/*iniciaTabuleiro(Linhas,Colunas,Tabul),*/
 	/*preenche os vizinhos de cada mina (lista de vizinhos) - preencheVizinhança*/
